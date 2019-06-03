757ab474870454b6a702cbe9c71252505ee82770
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-06-03 11:34:27 -0600

    adds testing for getting an address from a location

--- spec/services/google_service_spec.rb
+++ spec/services/google_service_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 describe GoogleService do
   it 'gets can return the long and lat of a given location' do
     address = 'Denver,co'
-    mapbox_service = GoogleService.new
-    result = mapbox_service.get_geo_coord(address)
+    google_service = GoogleService.new
+    result = google_service.get_geo_coord(address)
 
     expect(result).to be_a(Hash)
     expect(result[:results].first[:geometry][:location]).to eq({lat: 39.7392358, lng: -104.990251})
+  end
+
+  it 'returns an address for given lat and long of a location' do
+    latlng = {lat: 27, long: -82}
+    google_service = GoogleService.new
+    result = google_service.get_address(latlng)
+
+    expect(result).to be_a(Hash)
+    expect(result[:results].first[:address_components][3][:short_name]).to eq('Charlotte County')
   end
 end
