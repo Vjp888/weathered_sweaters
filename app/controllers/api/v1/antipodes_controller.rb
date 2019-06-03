8603e7ecb6979f6df25a83978277fb6edd084b4f
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-06-03 11:35:42 -0600

    reformats endpoint to use serializer and facade

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,17 +1,6 @@
 class Api::V1::AntipodesController < ApplicationController
   def show
-    geo_coords = collect_location(params[:loc])
-    
-    binding.pry
-  end
-
-  private
-
-  def collect_location(params)
-    geo_code = GoogleService.new
-    geo_coords = geo_code.get_geo_coord(params)
-    total_data = geo_coords[:results].first[:geometry][:location]
-    total_data[:location] = geo_coords[:results].first[:formatted_address]
-    total_data
+    antipode = AntipodeFacade.new(params[:loc])
+    render json: AntipodeSerializer.new(antipode)
   end
 end
