da3859b3d20ac2291336f789bb91d2fdf747de3b
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-06-05 04:51:11 -0600

    Creates migration for favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_050743) do
+ActiveRecord::Schema.define(version: 2019_06_05_082203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2019_06_05_050743) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_favorites_on_city_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -32,4 +41,6 @@ ActiveRecord::Schema.define(version: 2019_06_05_050743) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "cities"
+  add_foreign_key "favorites", "users"
 end
