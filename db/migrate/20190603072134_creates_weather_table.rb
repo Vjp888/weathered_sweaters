fdd1c22f5a4e7c76361c8045a4f9a46a2b840d01
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-06-04 23:51:16 -0600

    adds user and weather table to user creation and cachine json of weather

---
+++ db/migrate/20190603072134_creates_weather_table.rb
@@ -0,0 +1,12 @@
+class CreatesWeatherTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cities do |t|
+      t.text :city_name
+      t.float :latitude
+      t.float :longitude
+      t.json :weather
+
+      t.timestamps
+    end
+  end
+end
