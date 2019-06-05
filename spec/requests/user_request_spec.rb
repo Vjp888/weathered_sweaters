9dacefe42c1cc16c262fd42128bc35e7315472ff
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-06-05 00:01:15 -0600

    adds sad path test for when data is not sent correctly

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -16,4 +16,17 @@ describe 'When I visit /users' do
     expect(data['api_key']).to_not be_nil
     expect(user.api_key).to eq(data['api_key'])
   end
+
+  it 'returns an error if incorrect' do
+    post '/api/v1/users', params: {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "ossdnasd"
+    }
+
+    data = JSON.parse(response.body)
+
+    expect(response.code).to eq('400')
+    expect(data['errors'].first).to eq("Password confirmation doesn't match Password")
+  end
 end
